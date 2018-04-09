a646f5467117d1ea8ab2d2e0a1c0bd288bd9765e
bell <bell.sph@gmail.com>
2018-04-10 00:17:50 +0700

    merge develop

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM python:3
-RUN mkdir /app
-WORKDIR /app
-ADD . /app
-RUN ls -l
-RUN pip install -r reqs.txt
--- django_tutorial/django_tutorial/settings.py
+++ django_tutorial/django_tutorial/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,14 @@ WSGI_APPLICATION = 'django_tutorial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
